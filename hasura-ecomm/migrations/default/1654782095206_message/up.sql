ad60ac773cef8e1022a980b3eb45ab080ffe6f0a
Kristi-12 <kmengri@unitsystem.net>
2022-06-09 13:43:11 +0000

    created the message migration

---
+++ hasura-ecomm/migrations/default/1654782095206_message/up.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS "public"."message"(
+
+"id" TEXT,
+
+"user_id" INTEGER NOT NULL,
+
+"message_text" TEXT NOT NULL,
+
+"chatroom_id" INTEGER NOT NULL,
+
+"timestamp" DATE,
+
+CONSTRAINT "message_pkey" PRIMARY KEY ("id")
+);
